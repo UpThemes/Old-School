f7e8797938eac6bc5936db32e1816a5b27a5faf5
Chip Bennett <chip@chipbennett.net>
2011-12-10 09:43:58 -0600

    Proper hooking of Theme setup and Widgets init functions, proper function namespacing, proper referencing of template directory resources, proper script and stylesheet enqueueing

--- functions.php
+++ functions.php
@@ -1,6 +1,125 @@
 <?php
 
-wp_enqueue_script('jquery');
+
+function oldschool_setup_theme() {
+
+	add_theme_support( 'nav-menus' );
+	
+	register_nav_menus( array(
+		'primary_nav' => 'Primary Navigation',
+		'secondary_nav' => 'Secondary Navigation'
+	) );
+
+	add_theme_support('post-thumbnails');
+
+	add_image_size('featured-slider',980,240,1);
+	add_image_size('featured-thumb',300,250,1);
+	add_image_size('post-image',180,180,1);
+}
+add_action( 'after_setup_theme', 'oldschool_setup_theme' );
+
+
+function oldschool_widgets_init() {
+
+	// Register Widgetized areas.
+	// Area 1
+    register_sidebar( array(
+       	'name' => 'Left Column',
+       	'id' => 'left-column',
+       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
+       	'after_widget' => "</li>",
+		'before_title' => "<h3 class=\"widgettitle\">",
+		'after_title' => "</h3>\n",
+    ) );
+
+	// Area 2
+    register_sidebar( array(
+       	'name' => 'Right Column',
+       	'id' => 'right-column', 
+       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
+       	'after_widget' => "</li>",
+		'before_title' => "<h3 class=\"widgettitle\">",
+		'after_title' => "</h3>\n",
+    ) );
+
+	// Footer
+    register_sidebar( array(
+       	'name' => 'Footer',
+       	'id' => 'footer-area', 
+       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
+       	'after_widget' => "</li>",
+		'before_title' => "<h3 class=\"widgettitle\">",
+		'after_title' => "</h3>\n",
+    ) );
+}
+add_action( 'widgets_init', 'oldschool_widgets_init' );
+
+
+function oldschool_enqueue_styles() {
+
+	// Define stylesheet path
+	$oldschool_css_path = get_template_directory_uri( '/' );
+	// Enqueue print stylesheet
+	wp_enqueue_style(
+		'oldschool-print',
+		$oldschool_css_path . '/print.css',
+		array(),
+		false,
+		'print'
+	);
+	// Enqueue IE6 stylesheet
+	wp_enqueue_style(
+		'oldschool-ie6',
+		$ap_css_path . '/css/ie6.css',
+		array(),
+		false,
+		'screen'
+	);
+	// Enqueue IE7 stylesheet
+	wp_enqueue_style(
+		'oldschool-ie7',
+		$ap_css_path . '/css/ie7.css',
+		array(),
+		false,
+		'screen'
+	);
+	// Add IE conditionals
+	global $wp_styles;
+	$wp_styles->add_data( 'oldschool-ie6', 'conditional', 'IE 6' );
+	$wp_styles->add_data( 'oldschool-ie7', 'conditional', 'IE 7' );
+}
+add_action( 'wp_enqueue_scripts', 'oldschool_enqueue_styles' );
+
+
+function oldschool_enqueue_scripts() {
+	if ( ! is_admin() ) {
+		// Enqueue jQuery
+		wp_enqueue_script('jquery');
+		// Enqueue custom menu script
+		wp_enqueue_script(
+			'oldschool-menu',
+			get_template_directory_uri() . '/js/menu.js',
+			array( 'jquery' )
+		);
+		// Enqueue comment-reply script
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'oldschool_enqueue_scripts' );
+
+
+function oldschool_filter_body_class( $classes ) {
+	if ( is_singular() ) {
+		global $post;
+		$classes[] = $post->post_name;
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'oldschool_filter_body_class' );
+
+
 
 function above_content_ads(){
 	global $up_options;
@@ -11,6 +130,7 @@ function above_content_ads(){
     <?php } 
 }
 
+
 function below_content_ads(){
 	global $up_options;
     if($up_options->below_content_ads){ ?>
@@ -20,53 +140,8 @@ function below_content_ads(){
     <?php }
 }
 
-function widgets_init() {
-	if ( !function_exists('register_sidebars') )
-		return;
 
-	// Register Widgetized areas.
-	// Area 1
-    register_sidebar(array(
-       	'name' => 'Left Column',
-       	'id' => 'left-column',
-       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
-       	'after_widget' => "</li>",
-		'before_title' => "<h3 class=\"widgettitle\">",
-		'after_title' => "</h3>\n",
-    ));
-
-	// Area 2
-    register_sidebar(array(
-       	'name' => 'Right Column',
-       	'id' => 'right-column', 
-       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
-       	'after_widget' => "</li>",
-		'before_title' => "<h3 class=\"widgettitle\">",
-		'after_title' => "</h3>\n",
-    ));
-
-	// Area 2
-    register_sidebar(array(
-       	'name' => 'Footer',
-       	'id' => 'footer-area', 
-       	'before_widget' => '<li id="%1$s" class="widgetcontainer %2$s">',
-       	'after_widget' => "</li>",
-		'before_title' => "<h3 class=\"widgettitle\">",
-		'after_title' => "</h3>\n",
-    ));
-
-	add_theme_support( 'nav-menus' );
-	
-	if(function_exists('register_nav_menu')):
-	
-		register_nav_menu( 'primary_nav' , 'Primary Navigation' );
-		register_nav_menu( 'secondary_nav' , 'Secondary Navigation' );
-	
-	endif;
-
-}
-
-function up_page_menu(){
+function oldschool_page_menu(){
 	
 	echo "<ul id='subnav'>";
 
@@ -80,7 +155,7 @@ function up_page_menu(){
 
 }
 
-function up_category_menu(){
+function oldschool_category_menu(){
 	
 	echo "<ul id='nav' class='menu'>";
 
@@ -90,15 +165,6 @@ function up_category_menu(){
 		
 
 }
-
-// Runs our code at the end to check that everything needed has loaded
-add_action( 'init', 'widgets_init' );
-
-add_theme_support('post-thumbnails');
-
-add_image_size('featured-slider',980,240,1);
-add_image_size('featured-thumb',300,250,1);
-add_image_size('post-image',180,180,1);
 
 // Bootstrap the UpThemes Framework
 include("admin/admin.php");
--- header.php
+++ header.php
@@ -1,93 +1,72 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html <?php language_attributes(); ?>>
 <head>
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<?php
-/* Title Function */
-    if(function_exists('up_title')):
-        echo "<title>".up_title()."</title>";
-    else:
-        echo "<title>";
-        wp_title('');
-        if(!is_home())echo ' - '.get_bloginfo('name');
-        echo "</title>";
-    endif;
-    
-    /* SEO */
-    do_action('up_seo');
-?>
- 
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory') ?>/style.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory') ?>/print.css" media="print" />
-<link rel="start" href="<?php bloginfo('url'); ?>" title="Home" />
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<?php
+	/* Title Function */
+		if(function_exists('up_title')):
+			echo "<title>".up_title()."</title>";
+		else:
+			echo "<title>";
+			wp_title('');
+			if(!is_home())echo ' - '.get_bloginfo('name');
+			echo "</title>";
+		endif;
+		
+		/* SEO */
+		do_action('up_seo');
+	?>
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" media="screen" />
+	<link rel="start" href="<?php bloginfo('url'); ?>" title="Home" />
 
-<!--[if IE 6]>
-<script src="<?php bloginfo('stylesheet_directory') ?>/js/DD_belatedPNG.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory') ?>/ie6.css" media="screen" />
-<script type="text/javascript">
-DD_belatedPNG.fix("body,#logo,input.s,#maincontent .postwrapper,#sidebar-content .column h3,.commentlist .comment-feed,.commentlist,.commentlist li,#footer h3,.submit,.powered-by,#footertext,.twitter-avatar img");
-</script>
-<![endif]-->
+	<!--[if IE 6]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/DD_belatedPNG.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	DD_belatedPNG.fix("body,#logo,input.s,#maincontent .postwrapper,#sidebar-content .column h3,.commentlist .comment-feed,.commentlist,.commentlist li,#footer h3,.submit,.powered-by,#footertext,.twitter-avatar img");
+	</script>
+	<![endif]-->
 
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory') ?>/ie7.css" media="screen" />
-<![endif]-->
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php upfw_rss(); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php upfw_rss(); ?>" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php
 
-<?php
+	global $up_options;
+	?>
 
-global $up_options;
-
-if (is_singular()) {
-	wp_enqueue_script( 'comment-reply' );
-}
-
-?>
-
-<script language="javascript" type="text/javascript" src="<?php bloginfo('stylesheet_directory') ?>/js/menu.js"></script>
-
-<?php wp_head(); ?>
-
+	<?php wp_head(); ?>
 </head>
-<body <?php if (is_home()) { ?>id="home"<?php } else { ?>id="interior" class="<?php echo $post->post_name; ?>"<?php } ?>>
+<?php $body_id = ( is_home() ? "home" : "interior" ); ?>
+<body id="<?php echo $body_id; ?>" <?php body_class(); ?>>
 
   <div id="container">
 		<div id="header">
 	        <?php
-			
-			if(function_exists('wp_nav_menu')):
-							   
-			wp_nav_menu(array(
+
+			wp_nav_menu( array(
 				'theme_location' => 'secondary_nav',
 				'menu_class' => 'menu',
 				'menu_id' => 'subnav',
 				'container' => false,
-				'fallback_cb' => 'up_page_menu'
-			));
+				'fallback_cb' => 'oldschool_page_menu'
+			) );
 			
-			else: ?>
-        	<?php up_page_menu( 'pages', 8 ); ?>
-            <?php endif; ?>
-            
-            <?php
+			if( ! $up_options->hide_header_images ) :
 			
-			if( !$up_options->hide_header_images ):
-			
-				$thumbs = new WP_Query('showposts=10&posts_per_page=10&order=rand');
-				if($thumbs->have_posts()): 
-				?>
+				$thumbs = new WP_Query( 'showposts=10&posts_per_page=10&order=rand' );
+				if( $thumbs->have_posts() ) : 
+					?>
 	
 				<ul class="thumbs">
 	
-				<?php while ($thumbs->have_posts()) : $thumbs->the_post(); 
+				<?php while ( $thumbs->have_posts() ) : $thumbs->the_post(); 
 	
-					if(has_post_thumbnail()):
+					if( has_post_thumbnail() ) :
 					?>
 					<li><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title(); ?>">
 		
-						<?php echo the_post_thumbnail(array(32,32)); ?>
+						<?php echo the_post_thumbnail( array( 32, 32 ) ); ?>
 					
 					</a></li>
 					<?php
@@ -103,27 +82,23 @@ if (is_singular()) {
 				
 			?>
             
-			<h1><a href="<?php bloginfo('url'); ?>">
-            	<?php if($up_options->logo): ?>
-                	<img id="logo" src="<?php echo $up_options->logo; ?>" alt="<?php bloginfo('name'); ?>" /></a>
+			<h1><a href="<?php echo home_url(); ?>">
+            	<?php if ( $up_options->logo ) : ?>
+                	<img id="logo" src="<?php echo $up_options->logo; ?>" alt="<?php bloginfo( 'name' ); ?>" /></a>
                 <?php else: ?>
-                	<img id="logo" src="<?php bloginfo('stylesheet_directory') ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" /></a>
+                	<img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
                 
                 <?php endif; ?>
             </h1>
 	        <?php
-			if(function_exists('wp_nav_menu')):
-							   
-			wp_nav_menu(array(
+			
+			wp_nav_menu( array(
 				'theme_location' => 'primary_nav',
 				'menu_class' => 'menu',
 				'menu_id' => 'nav',
 				'container' => false,
-				'fallback_cb' => 'up_category_menu'
-			));
-			
-			else: ?>
-        	<?php up_category_menu(); ?>
-            <?php endif; ?>
+				'fallback_cb' => 'oldschool_category_menu'
+			) );
+			?>
 
 		</div>
